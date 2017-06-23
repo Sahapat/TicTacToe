c6387fec3c8a92d1a979df68c5d6069097f9e3b9
Sahapat <sahapat.starcaft@gmail.com>
2017-06-23 13:25:03 +0700

    Create TableClass

---
+++ TicTacToe/XoTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class XoTable
+    {
+        private bool[,] _Table = new bool[3, 3];
+        public bool[,] Table
+        {
+            get
+            {
+                return _Table;
+            }
+            set
+            {
+                _Table = value;
+            }
+        }
+        
+    }
+}
