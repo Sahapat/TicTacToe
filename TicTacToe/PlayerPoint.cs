752a473e2fd57a6e9095cd5f9d1cd092a35256c2
Sahapat <sahapat.starcaft@gmail.com>
2017-06-24 11:15:01 +0700

    Create Struct Playerpoint

---
+++ TicTacToe/PlayerPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+     struct PlayerStatus
+    {
+        public short health;
+        public bool round;
+    }
+    interface ITicTacToeTable
+    {
+        void Display();
+        bool CheckScore();
+    }
+}
