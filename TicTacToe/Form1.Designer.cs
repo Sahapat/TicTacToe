91911f511aba4959a8e47635b2e18396f4cb8310
Sahapat <sahapat.starcaft@gmail.com>
2017-07-19 01:05:15 +0700

    Add button

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Block1 = new System.Windows.Forms.Button();
+            this.Block2 = new System.Windows.Forms.Button();
+            this.Block3 = new System.Windows.Forms.Button();
+            this.Block6 = new System.Windows.Forms.Button();
+            this.Block5 = new System.Windows.Forms.Button();
+            this.Block4 = new System.Windows.Forms.Button();
+            this.Block9 = new System.Windows.Forms.Button();
+            this.Block8 = new System.Windows.Forms.Button();
+            this.Block7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Block1
+            // 
+            this.Block1.Location = new System.Drawing.Point(12, 17);
+            this.Block1.Name = "Block1";
+            this.Block1.Size = new System.Drawing.Size(126, 117);
+            this.Block1.TabIndex = 0;
+            this.Block1.UseVisualStyleBackColor = true;
+            this.Block1.Click += new System.EventHandler(this.Block1_Click);
+            // 
+            // Block2
+            // 
+            this.Block2.Location = new System.Drawing.Point(161, 17);
+            this.Block2.Name = "Block2";
+            this.Block2.Size = new System.Drawing.Size(126, 117);
+            this.Block2.TabIndex = 1;
+            this.Block2.UseVisualStyleBackColor = true;
+            this.Block2.Click += new System.EventHandler(this.Block2_Click);
+            // 
+            // Block3
+            // 
+            this.Block3.Location = new System.Drawing.Point(309, 17);
+            this.Block3.Name = "Block3";
+            this.Block3.Size = new System.Drawing.Size(126, 117);
+            this.Block3.TabIndex = 2;
+            this.Block3.UseVisualStyleBackColor = true;
+            this.Block3.Click += new System.EventHandler(this.Block3_Click);
+            // 
+            // Block6
+            // 
+            this.Block6.Location = new System.Drawing.Point(309, 148);
+            this.Block6.Name = "Block6";
+            this.Block6.Size = new System.Drawing.Size(126, 117);
+            this.Block6.TabIndex = 5;
+            this.Block6.UseVisualStyleBackColor = true;
+            this.Block6.Click += new System.EventHandler(this.Block6_Click);
+            // 
+            // Block5
+            // 
+            this.Block5.Location = new System.Drawing.Point(161, 148);
+            this.Block5.Name = "Block5";
+            this.Block5.Size = new System.Drawing.Size(126, 117);
+            this.Block5.TabIndex = 4;
+            this.Block5.UseVisualStyleBackColor = true;
+            this.Block5.Click += new System.EventHandler(this.Block5_Click);
+            // 
+            // Block4
+            // 
+            this.Block4.Location = new System.Drawing.Point(12, 148);
+            this.Block4.Name = "Block4";
+            this.Block4.Size = new System.Drawing.Size(126, 117);
+            this.Block4.TabIndex = 3;
+            this.Block4.UseVisualStyleBackColor = true;
+            this.Block4.Click += new System.EventHandler(this.Block4_Click);
+            // 
+            // Block9
+            // 
+            this.Block9.Location = new System.Drawing.Point(309, 281);
+            this.Block9.Name = "Block9";
+            this.Block9.Size = new System.Drawing.Size(126, 117);
+            this.Block9.TabIndex = 8;
+            this.Block9.UseVisualStyleBackColor = true;
+            this.Block9.Click += new System.EventHandler(this.Block9_Click);
+            // 
+            // Block8
+            // 
+            this.Block8.Location = new System.Drawing.Point(161, 281);
+            this.Block8.Name = "Block8";
+            this.Block8.Size = new System.Drawing.Size(126, 117);
+            this.Block8.TabIndex = 7;
+            this.Block8.UseVisualStyleBackColor = true;
+            this.Block8.Click += new System.EventHandler(this.Block8_Click);
+            // 
+            // Block7
+            // 
+            this.Block7.Location = new System.Drawing.Point(12, 281);
+            this.Block7.Name = "Block7";
+            this.Block7.Size = new System.Drawing.Size(126, 117);
+            this.Block7.TabIndex = 6;
+            this.Block7.UseVisualStyleBackColor = true;
+            this.Block7.Click += new System.EventHandler(this.Block7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 416);
+            this.Controls.Add(this.Block9);
+            this.Controls.Add(this.Block8);
+            this.Controls.Add(this.Block7);
+            this.Controls.Add(this.Block6);
+            this.Controls.Add(this.Block5);
+            this.Controls.Add(this.Block4);
+            this.Controls.Add(this.Block3);
+            this.Controls.Add(this.Block2);
+            this.Controls.Add(this.Block1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Block1;
+        private System.Windows.Forms.Button Block2;
+        private System.Windows.Forms.Button Block3;
+        private System.Windows.Forms.Button Block6;
+        private System.Windows.Forms.Button Block5;
+        private System.Windows.Forms.Button Block4;
+        private System.Windows.Forms.Button Block9;
+        private System.Windows.Forms.Button Block8;
+        private System.Windows.Forms.Button Block7;
     }
 }
 
