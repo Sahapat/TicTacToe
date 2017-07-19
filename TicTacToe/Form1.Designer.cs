870709f260f681929a8e929f6631da10a20292c6
Sahapat <sahapat.starcaft@gmail.com>
2017-07-19 16:07:06 +0700

    Fix button size

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -41,90 +41,99 @@
             // 
             // Block1
             // 
-            this.Block1.Location = new System.Drawing.Point(12, 17);
+            this.Block1.Location = new System.Drawing.Point(9, 14);
+            this.Block1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Block1.Name = "Block1";
-            this.Block1.Size = new System.Drawing.Size(126, 117);
+            this.Block1.Size = new System.Drawing.Size(94, 95);
             this.Block1.TabIndex = 0;
             this.Block1.UseVisualStyleBackColor = true;
             this.Block1.Click += new System.EventHandler(this.Block1_Click);
             // 
             // Block2
             // 
-            this.Block2.Location = new System.Drawing.Point(161, 17);
+            this.Block2.Location = new System.Drawing.Point(121, 14);
+            this.Block2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Block2.Name = "Block2";
-            this.Block2.Size = new System.Drawing.Size(126, 117);
+            this.Block2.Size = new System.Drawing.Size(94, 95);
             this.Block2.TabIndex = 1;
             this.Block2.UseVisualStyleBackColor = true;
             this.Block2.Click += new System.EventHandler(this.Block2_Click);
             // 
             // Block3
             // 
-            this.Block3.Location = new System.Drawing.Point(309, 17);
+            this.Block3.Location = new System.Drawing.Point(232, 14);
+            this.Block3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Block3.Name = "Block3";
-            this.Block3.Size = new System.Drawing.Size(126, 117);
+            this.Block3.Size = new System.Drawing.Size(94, 95);
             this.Block3.TabIndex = 2;
             this.Block3.UseVisualStyleBackColor = true;
             this.Block3.Click += new System.EventHandler(this.Block3_Click);
             // 
             // Block6
             // 
-            this.Block6.Location = new System.Drawing.Point(309, 148);
+            this.Block6.Location = new System.Drawing.Point(232, 120);
+            this.Block6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Block6.Name = "Block6";
-            this.Block6.Size = new System.Drawing.Size(126, 117);
+            this.Block6.Size = new System.Drawing.Size(94, 95);
             this.Block6.TabIndex = 5;
             this.Block6.UseVisualStyleBackColor = true;
             this.Block6.Click += new System.EventHandler(this.Block6_Click);
             // 
             // Block5
             // 
-            this.Block5.Location = new System.Drawing.Point(161, 148);
+            this.Block5.Location = new System.Drawing.Point(121, 120);
+            this.Block5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Block5.Name = "Block5";
-            this.Block5.Size = new System.Drawing.Size(126, 117);
+            this.Block5.Size = new System.Drawing.Size(94, 95);
             this.Block5.TabIndex = 4;
             this.Block5.UseVisualStyleBackColor = true;
             this.Block5.Click += new System.EventHandler(this.Block5_Click);
             // 
             // Block4
             // 
-            this.Block4.Location = new System.Drawing.Point(12, 148);
+            this.Block4.Location = new System.Drawing.Point(9, 120);
+            this.Block4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Block4.Name = "Block4";
-            this.Block4.Size = new System.Drawing.Size(126, 117);
+            this.Block4.Size = new System.Drawing.Size(94, 95);
             this.Block4.TabIndex = 3;
             this.Block4.UseVisualStyleBackColor = true;
             this.Block4.Click += new System.EventHandler(this.Block4_Click);
             // 
             // Block9
             // 
-            this.Block9.Location = new System.Drawing.Point(309, 281);
+            this.Block9.Location = new System.Drawing.Point(232, 228);
+            this.Block9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Block9.Name = "Block9";
-            this.Block9.Size = new System.Drawing.Size(126, 117);
+            this.Block9.Size = new System.Drawing.Size(94, 95);
             this.Block9.TabIndex = 8;
             this.Block9.UseVisualStyleBackColor = true;
             this.Block9.Click += new System.EventHandler(this.Block9_Click);
             // 
             // Block8
             // 
-            this.Block8.Location = new System.Drawing.Point(161, 281);
+            this.Block8.Location = new System.Drawing.Point(121, 228);
+            this.Block8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Block8.Name = "Block8";
-            this.Block8.Size = new System.Drawing.Size(126, 117);
+            this.Block8.Size = new System.Drawing.Size(94, 95);
             this.Block8.TabIndex = 7;
             this.Block8.UseVisualStyleBackColor = true;
             this.Block8.Click += new System.EventHandler(this.Block8_Click);
             // 
             // Block7
             // 
-            this.Block7.Location = new System.Drawing.Point(12, 281);
+            this.Block7.Location = new System.Drawing.Point(9, 228);
+            this.Block7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Block7.Name = "Block7";
-            this.Block7.Size = new System.Drawing.Size(126, 117);
+            this.Block7.Size = new System.Drawing.Size(94, 95);
             this.Block7.TabIndex = 6;
             this.Block7.UseVisualStyleBackColor = true;
             this.Block7.Click += new System.EventHandler(this.Block7_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(446, 416);
+            this.ClientSize = new System.Drawing.Size(334, 338);
             this.Controls.Add(this.Block9);
             this.Controls.Add(this.Block8);
             this.Controls.Add(this.Block7);
@@ -134,6 +143,7 @@
             this.Controls.Add(this.Block3);
             this.Controls.Add(this.Block2);
             this.Controls.Add(this.Block1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
